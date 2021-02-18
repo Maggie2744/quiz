34f46cb7ebdc274b87815f53f72853e1ca8c28ab
Maggie <margaret.hotte@gmail.com>
2021-02-18 11:05:48 -0500

    Updated the Test code

---
+++ questions.js
@@ -0,0 +1,36 @@
+const questions = [
+  {
+    "question": "What is your goal?",
+    "answer1": "Portfolio Review",
+    "answer1Total": "1",
+    "answer2": "Find An Available Designer",
+    "answer2Total": "2",
+    "answer3": "Networking",
+    "answer3Total": "3"
+  },
+  {
+    "question": "What area of design / specialization are you in search of?",
+    "answer1": "Web Design",
+    "answer1Total": "1",
+    "answer2": "UI & UX",
+    "answer2Total": "2",
+    "answer3": "Branding",
+    "answer3Total": "3"
+    "answer4": "Motion Design"
+    "answer4Total": "4"
+    "answer5": "Illustration"
+    "answer5Total": "5"
+    "answer6": "Print Design"
+    "answer6Total": "6"
+  },
+  {
+    "question":
+      "What key attributes are you looking for in a designer?<br>(you may select multiple options)",
+    "answer1": "Love, Career, Money",
+    "answer1Total": "1",
+    "answer2": "Money, Career, Love",
+    "answer2Total": "3",
+    "answer3": "Career, Love, Money",
+    "answer3Total": "2"
+  }
+]
