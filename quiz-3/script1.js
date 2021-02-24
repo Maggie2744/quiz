73f46d21faa19d9c9974e05fbf85f3ed29182175
Maggie <margaret.hotte@gmail.com>
2021-02-24 13:28:27 -0500

    Updated

---
+++ quiz-3/script1.js
@@ -0,0 +1,30 @@
+// external js: isotope.pkgd.js
+
+// init Isotope
+var $grid = $('.grid').isotope({
+  itemSelector: '.designer'
+});
+
+// store filter for each group
+var filters = {};
+
+$('.filters').on( 'change', function( event ) {
+  var $select = $( event.target );
+  // get group key
+  var filterGroup = $select.attr('value-group');
+  // set filter for group
+  filters[ filterGroup ] = event.target.value;
+  // combine filters
+  var filterValue = concatValues( filters );
+  // set filter for Isotope
+  $grid.isotope({ filter: filterValue });
+});
+
+// flatten object by concatting values
+function concatValues( obj ) {
+  var value = '';
+  for ( var prop in obj ) {
+    value += obj[ prop ];
+  }
+  return value;
+}
