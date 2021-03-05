638c06b0fbdea059b1340d001296307da4d164fc
Maggie <margaret.hotte@gmail.com>
2021-03-05 00:39:53 -0500

    Created a good copy
    in folder Quiz Test 6, this is the good copy of the quiz

---
+++ Quiz Test 2/questions.js
@@ -0,0 +1,54 @@
+const questions = [
+  {
+    "question": "What is your goal?",
+    "answer1": "Portfolio Review",
+    "answer1Total": "1",
+    "answer2": "Job Request",
+    "answer2Total": "2",
+    "answer3": "Networking",
+    "answer3Total": "3"
+  },
+  {
+    "question": "What area of design / specialization are you in search of?",
+    "answer1": "Web Design",
+    "answer1Total": "1",
+    "answer2": "UI & UX",
+    "answer2Total": "2",
+    "answer3": "Branding",
+    "answer3Total": "3"
+    "answer4": "Motion Design"
+    "answer4Total": "4"
+    "answer5": "Illustration"
+    "answer5Total": "5"
+    "answer6": "Print Design"
+    "answer6Total": "6"
+  },
+  {
+    "question":
+      "What key attributes are you looking for in a designer?<br>(you may select multiple options)",
+    "answer1": "Altruistic",
+    "answer1Total": "1",
+    "answer2": "Communicative",
+    "answer2Total": "2",
+    "answer3": "Conscientious",
+    "answer3Total": "3"
+    "answer4Total": "Continuous Learner",
+    "answer4Total": "4",
+    "answer5Total": "Detail-Oriented",
+    "answer5Total": "5",
+    "answer6Total": "Efficient",
+    "answer6Total": "6"
+    "answer7": "Enthusiastic",
+    "answer7Total": "7",
+    "answer8": "Flexible",
+    "answer8Total": "8",
+    "answer9": "Goal-Oriented",
+    "answer9Total": "9"
+    "answer10": "Independent",
+    "answer10Total": "10",
+    "answer11": "Strategic",
+    "answer11Total": "11",
+    "answer12": "Team-Oriented",
+    "answer12Total": "12"
+  }
+]
---
+++ Quiz Test 2/test.js
@@ -0,0 +1,8 @@
+if($('ul#filter_list').length){		// Check to see if the ul news filter exists...
+		$('ul#filter_list a').click(function(e){	// When click on filter button...
+			e.preventDefault();		// Disables anchor tag so that it won't go back to the top of the page.
+			var className = $(this).attr('class').split(" ")[0];		// Assigns the tag (the FIRST class in the anchor tag)
+			$('ul#filter_list a.'+ className).toggleClass('active');  // Toggles checkmark on ALL filter anchor tags w/ that id on the page (in case the page has multiple )
+			$('article.' + className).toggleClass('hide');  // Toggles hide class for all articles of selected school
+		});//END $('a.lcas_button').click
+}
